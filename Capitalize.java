0c5b9b21c88baa32f66acac4de1d8922db83788b
VALASALARAKESH <valasalarakesh1254@gmail.com>
2022-10-15 15:11:01 +0530

    The Capitalize.java program is modified as per your requirements

--- Capitalize.java
+++ Capitalize.java
@@ -1,19 +1,14 @@
-class Main {
+import  java.util.Scanner;
+class Capitalize {
     public static void main(String[] args) {
-
-
-        String name = "hello welcome to hacktoberfest!";
-
-
+        Scanner input=new Scanner(System.in);
+        System.out.println("Please enter the string: ");
+        String name = input.nextLine();
         String firstLetter = name.substring(0, 1);
         String remainingLetters = name.substring(1, name.length());
-
-
         firstLetter = firstLetter.toUpperCase();
-
-
         name = firstLetter + remainingLetters;
-        System.out.println("Name: " + name);
+        System.out.println( name);
 
     }
 }
